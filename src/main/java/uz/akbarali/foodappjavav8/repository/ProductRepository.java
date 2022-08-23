aa2712ca32d3ffc1047cdf845a5a67695423a971
Akbarali Asqaraliyev <akbaraliasqaraliyev0610@gmail.com>
2022-08-23 18:21:04 +0500

    dsdfh

--- src/main/java/uz/akbarali/foodappjavav8/repository/ProductRepository.java
+++ src/main/java/uz/akbarali/foodappjavav8/repository/ProductRepository.java
@@ -22,8 +22,8 @@ public interface ProductRepository extends BaseRepository<Product> {
     @Query(nativeQuery = true,
             value = "select cast(p.id as varchar) as id,\n" +
                     "       name_uz               as name,\n" +
-                    "       price,\n" +
-                    "       ac.data as image\n" +
+                    "       price\n" +
+//                    "       ac.data as image\n" +
                     "from product p\n" +
                     "         join attachments a on a.id = p.attachment_id\n" +
                     "         join attachment_contents ac on a.id = ac.attachment_id\n" +
